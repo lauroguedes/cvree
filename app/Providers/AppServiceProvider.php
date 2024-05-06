8e7bc785ff2a57d58b25d3c869051cd4183dfa16
Lauro Guedes <leowgweb@gmail.com>
2024-05-06 21:04:09 +0100

    feat(template): Create header and update structure insight template

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use TallStackUi\Facades\TallStackUi;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        TallStackUi::personalize()
+            ->avatar()
+            ->block('wrapper.sizes.lg', 'size-32 text-2xl')
+            ->block('content.image.sizes.lg', 'size-24 text-2xl');
     }
 }
--- resources/views/livewire/pages/templates/insight.blade.php
+++ resources/views/livewire/pages/templates/insight.blade.php
@@ -1,5 +1,23 @@
 <div>
-    <header>{{ $user->name }}</header>
+    <header class="flex flex-col items-center space-y-4">
+        <x-tsavatar lg :model="$user"
+                    property="name"
+                    background="ff0000"
+                    color="fff"
+                    :options="['rounded' => true, 'size' => 128]"
+        />
+        <h1 class="uppercase text-2xl font-bold text-gray-500">{{ $user->name }}</h1>
+        <div>
+            <x-tsbadge icon="user" position="left" class="uppercase" text="Sr. Software Engineer" color="secondary" light />
+            <span>at</span>
+            <x-tsbadge icon="building-storefront" position="left" class="uppercase" text="Google" color="secondary" light />
+        </div>
+        <div class="flex space-x-2 text-gray-400">
+            <x-tsbadge icon="cake" sm position="left" text="32" color="white" light></x-tsbadge>
+            <x-tsbadge icon="map-pin" sm position="left" text="Cork, Ireland" color="white" light />
+            <x-tsbadge icon="device-phone-mobile" sm position="left" text="+353 9876432" color="white" light />
+        </div>
+    </header>
     <aside>{{ $user->email }}</aside>
     <main>content</main>
     <aside>right</aside>
