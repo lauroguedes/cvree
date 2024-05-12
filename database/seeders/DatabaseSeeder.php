97e4bf4065dfd1031320a9074d7804792c8ab614
Lauro Guedes <leowgweb@gmail.com>
2024-05-12 10:16:34 +0100

    feat(migration): Create username field on user table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(9)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Lauro Guedes',
+            'email' => 'me@user.com',
+            'password' => bcrypt('secret'),
+            'username' => 'lauroguedes',
         ]);
     }
 }
